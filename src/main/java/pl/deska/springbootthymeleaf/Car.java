ca19bd265ec5818d8910f47603df0b5572088046
Mateusz Deska <mateuszdeska00@gmail.com>
2020-10-27 01:04:30 +0100

    add element

--- src/main/java/pl/deska/springbootthymeleaf/Car.java
+++ src/main/java/pl/deska/springbootthymeleaf/Car.java
@@ -14,20 +14,20 @@ public class Car {
     public Car() {
     }
 
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public String getMark() {
         return mark;
     }
 
     public void setMark(String mark) {
         this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 
     @Override
